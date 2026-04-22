05bbbd0232490606147bd43d35802c992a840660
Kaiz <kaiz@kaiz.jp>
2026-04-22 13:46:59 +0900

    feat: add query helpers package

---
+++ packages/query/README.md
@@ -0,0 +1,3 @@
+# @gtfs-jp/query
+
+`@gtfs-jp/loader` の上に載る、GTFS-JP データ向けの再利用可能な read/query helper 集です。
---
+++ packages/query/src/index.ts
@@ -0,0 +1,12 @@
+export { getActiveServiceIds } from './service-date.js';
+export { getStopTimetableDetail } from './stop-timetable.js';
+
+export type {
+  GetActiveServiceIdsResult,
+  GtfsQuerySource,
+  LoadStopTimetableOptions,
+  StopInfo,
+  StopTimetableDetail,
+  StopTimetableGroup,
+  StopTimetableRow,
+} from './types.js';
